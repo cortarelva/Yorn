7980ceed45054e9b65d314d957de1a53db75e6e2
cortarelva <45341961+cortarelva@users.noreply.github.com>
2019-07-22 10:22:59 +0100

    cards done

--- js/jquery/main.js
+++ js/jquery/main.js
@@ -1,3 +1,24 @@
-$(function(){
-$('.p').css('color','red');
-});
+/*$(function(){
+  let val = $('.input').val();
+  let prog = $('.progress');
+  $('#assign').on('click',() =>{
+    alert(val);
+    prog.style.width = val;
+    
+  })
+  
+})*/
+
+function move() {
+  var elem = document.getElementsById("assign"); 
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
