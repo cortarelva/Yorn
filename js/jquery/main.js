76310bf159977d8f4a0fa11f6ee803b5e1e44438
cortarelva <45341961+cortarelva@users.noreply.github.com>
2019-08-02 17:15:56 +0100

    offerwall half done/slider done(jr)/tarifarios media queries 90% done

--- js/jquery/main.js
+++ js/jquery/main.js
@@ -1,1 +1,11 @@
 
+ $(document).ready(function(){
+    $('.slick-card-container').slick({
+      arrows:false,
+      centerMode:true,
+      centerPadding: '40px',
+      slidesToShow:1,
+      focusOnSelect:false,
+          
+    });
+  });
